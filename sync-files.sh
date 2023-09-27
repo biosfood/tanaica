3dbdf87435b6a69c5df159e8ea981740bc51d56a
biosfood <leisenhau@gmail.com>
2023-09-27 09:07:09 +0200

    sync script: copy files to repository

--- sync-files.sh
+++ sync-files.sh
@@ -6,5 +6,19 @@ if [[ $# != 1 ]]; then
 fi
 
 for file in $(cat $1); do
-  echo "syncing $file..."
+  if [[ $file =~ ~.* ]]; then
+    prefix="userhome"
+    filename=$(echo $file | sed "s/~//")
+    targetFile="$prefix$filename"
+    file="$HOME$filename"
+  elif [[ $file =~ \/.* ]]; then
+    prefix="root"
+    targetFile="$prefix$file"
+  else
+    echo "$file is not a usable path, skipping . . ."
+    continue
+  fi
+  echo "copying $file to $targetFile"
+  mkdir -p "$(dirname "$targetFile")"
+  /usr/bin/cp -r ${file} ${targetFile}
 done
