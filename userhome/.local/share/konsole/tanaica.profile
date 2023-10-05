3ea823bd426259c706fe0e1258a9da3f69cfa975
biosfood <leisenhau@gmail.com>
2023-10-05 20:43:10 +0200

    add script and configurations for konsole with colors from envvars

---
+++ userhome/.local/share/konsole/tanaica.profile
@@ -0,0 +1,7 @@
+[Appearance]
+ColorScheme=tanaica
+Font=Source Code Pro,14,-1,5,50,0,0,0,0,0
+
+[General]
+Name=Tanaica
+Parent=FALLBACK/
---
+++ userhome/.scripts/build-konsole-colors.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# apply tanaica colorscheme with envvars
+envsubst < ~/.local/share/konsole/tanaica.colorscheme.template > ~/.local/share/konsole/tanaica.colorscheme
