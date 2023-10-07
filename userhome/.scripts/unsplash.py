0f84d3a4e10699879773201cf4fe4d69328e8983
biosfood <leisenhau@gmail.com>
2023-10-07 16:06:34 +0200

    add script to fetch a random featured unsplash image as the background

---
+++ userhome/.scripts/unsplash.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+import requests
+import os
+
+baseURL = "https://api.unsplash.com"
+
+envname = "UNSPLASH_CLIENT_ID"
+if not (envname in os.environ):
+    exit(f"Please add {envname} to your environment")
+clientId = os.environ[envname]
+
+path = "~/.var/backgrounds/"
+if not os.path.isdir(os.path.expanduser(path)):
+    os.makedirs(os.path.expanduser(path))
+
+payload = {
+    "client_id": clientId,
+    "orientation": "landscape",
+    "collections": "317099",
+}
+
+response = requests.get(f"{baseURL}/photos/random", params=payload).json()
+url = response["urls"]["raw"]
+slug = response["slug"]
+
+filename = f"{path}{slug}.jpg"
+
+print(f"downloading {url} as {filename}...")
+
+with open(os.path.expanduser(filename), "wb+") as file:
+    file.write(requests.get(url).content)
+
+print("Finished downloading, setting the image as the desktop-backgroud now...")
+os.system(f"feh --bg-fill {filename}")
