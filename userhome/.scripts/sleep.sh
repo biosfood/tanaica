cbd640e7ae8492c4c174e9c6c5323007e8f70eda
biosfood <leisenhau@gmail.com>
2024-02-02 09:38:40 +0100

    lock keepassxc when putting the laptop to sleep

--- userhome/.scripts/sleep.sh
+++ userhome/.scripts/sleep.sh
@@ -1,3 +1,7 @@
 #/bin/sh
 
+# lock keepass xc databases
+dbus-send --print-reply --dest=org.keepassxc.KeePassXC.MainWindow /keepassxc org.keepassxc.MainWindow.lockAllDatabases
+
+# go to sleep
 systemctl suspend
